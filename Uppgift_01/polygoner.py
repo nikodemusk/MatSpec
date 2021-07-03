9dbe1c5c9ccdca26e088813d062860280339bd5a
Nikodemus Karlsson <nikodemus@nikodemus.se>
2021-07-03 22:01:21 +0200

    Kommenterat filen

--- Uppgift_01/polygoner.py
+++ Uppgift_01/polygoner.py
@@ -1,19 +1,20 @@
 import turtle as t
-tim = t.Turtle()
-tim.hideturtle()
-tim.pensize(5)
-tim.shape("triangle")
-tim.shapesize(0.5)
-tim.speed(0) # 0 - 10
+tim = t.Turtle() # Sköldpaddan kan döpas till vad som helst
+# tim.hideturtle() # Kan avkommenteras när programmet är klart
+tim.pensize(5.0) # Kan vara vilket tal x som helst, x > 0
+tim.shape("turtle")
+tim.shapesize(2.0) # Kan vara vilket tal x som helst, x > 0
+tim.speed(2) # Heltal 0 - 10 (1 - 10 ökande hastighet, 0 är snabbast)
 screen = t.Screen()
+screen.bgcolor("wheat1")
 
-def goto(x, y, mark=False):
+def move_to(x, y, mark=False):
     if not mark:
-        tim.penup()
+        tim.penup() # Normalt så ritas ett streck efter sköldpaddans väg, penup() ändrar på det
     tim.goto(x, y)
-    tim.pendown()
+    tim.pendown() # Glöm inte att sätta ned pennan igen där det behövs
 
-goto(-200, -100)
+move_to(-100, 50)
 
 # Nedanstående förflyttningar görs
 # med fördel i en loop.
@@ -28,6 +29,5 @@ tim.right(90)
 tim.forward(150)
 tim.right(90)
 tim.end_fill()
-
 
 screen.exitonclick()
